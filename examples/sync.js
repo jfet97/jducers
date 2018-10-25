35029ae465e10a680dde4ac046f2cc0b9c842e2f
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-10-25 13:26:45 +0200

    Update sync.js

--- examples/sync.js
+++ examples/sync.js
@@ -1,4 +1,4 @@
-import { pipe, compose } from './fp_core';
+import { pipe, compose } from 'jducers/utility';
 import * as J from './jducers'
 
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
